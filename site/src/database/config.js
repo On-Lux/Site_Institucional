c19b46a9f22d96f04dfda1177843cbf8ab27eb21
MaessioSptech <maessio.sousa@sptech.school>
2022-12-03 17:16:21 -0300

    credenciais do banco

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "Onlux",
-    user: "aluno",
-    password: "sptech",
+    user: "root",
+    password: "manodoido",
 };
 
 function executar(instrucao) {
