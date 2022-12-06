3393ef26f9dd6c4ffe4474489151c4ca46898df9
willpaternezi <willian.paternezi@sptech.school>
2022-12-05 23:36:55 -0300

    correcao deploy

--- site/app.js
+++ site/app.js
@@ -5,7 +5,7 @@
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
+var PORTA = process.env.PORT || 8080;
 
 var app = express();
 
